fcc4d29b1195eaa88b89a2115801d2ba245d9075
Ignacio Reyes <61832602+ignareyesa@users.noreply.github.com>
2022-05-10 16:55:30 +0200

    Update visuallook.js

--- visuallook.js
+++ visuallook.js
@@ -1025,51 +1025,36 @@ looker.plugins.visualizations.add({
   // Id and Label are legacy properties that no longer have any function besides documenting
   // what the visualization used to have. The properties are now set via the manifest
   // form within the admin/visualizations page of Looker
-  id: "visuallook",
-  label: "visuallook",
-  options: {
-    chart_type: {
-      type: "string",
-      label: "Chart type",
-      values: [
-        {"Bar": "Bar"},
-        {"Line": "Line"},
-        {"Pie": "Pie"}
-      ],
-      display: "radio",
-      default: "Bar"
+  id: 'some id', // id/label not required, but nice for testing and keeping manifests in sync
+    label: 'Some Name',
+    options: {
+        title: {
+            type: 'string',
+            label: 'Title',
+            display: 'text',
+            default: 'Default Text'
+        }
+    },
+    // Set up the initial state of the visualization
+    create(element, config) {
+        this.elementRef = element;
+    },
+    // Render in response to the data or settings changing
+    update(data, element, config, queryResponse) {
+        console.log( 'data', data );
+        console.log( 'element', element );
+        console.log( 'config', config );
+        console.log( 'queryResponse', queryResponse );
+        const errors = handleErrors(this, queryResponse, {
+            // min_pivots: 0,
+            // max_pivots: 0,
+            // min_dimensions: 1,
+            // max_dimensions: 1,
+            // min_measures: 1,
+            // max_measures: 1
+        });
+        if (errors) { // errors === true means no errors
+            element.innerHTML = 'Hello Looker!';
+        }
     }
-  },
-  // Set up the initial state of the visualization
-  create: function(element, config) {
-
-    // Insert a <style> tag with some styles we'll use later.
-  },
-  // Render in response to the data or settings changing
-  updateAsync: function(data, element, config, queryResponse, details, done) {
-    element.innerHTML = `<div id="chart">HOLA</div>`;
-
-    const svg = document.querySelector('#chart')
-    const dim = queryResponse.fields.dimensions[0].name
-    const meas = queryResponse.fields.measures[0].name
-    console.log("Dim = "+ dim)
-    console.log("Meas = "+ meas)
-    const options = {
-      data: {
-        labels: data.map((d)=> {
-          let label = ""
-          queryResponse.fields.dimensions.forEach((dim) => label += ` ${d[dim.name].value}`)
-          return label
-        }),
-        datasets: queryResponse.fields.measures.map((m) => {
-          return {
-            data: data.map((d) => console.log(d[m.name].value))
-          }
-        }),
-    }}
-    console.log(options)
-    //new chartXkcd[config.chart_type || "Bar"](svg, options);
-
-    done()
-  }
 });
