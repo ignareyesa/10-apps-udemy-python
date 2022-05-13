a7142e9e62451a3c85f48014e25fdb5383b691f7
Ignacio Reyes <61832602+ignareyesa@users.noreply.github.com>
2022-05-13 08:07:09 +0200

    Update visuallook.js

--- visuallook.js
+++ visuallook.js
@@ -2,8 +2,8 @@ looker.plugins.visualizations.add({
   // Id and Label are legacy properties that no longer have any function besides documenting
   // what the visualization used to have. The properties are now set via the manifest
   // form within the admin/visualizations page of Looker
-  id: 'some id', // id/label not required, but nice for testing and keeping manifests in sync
-    label: 'Some Name',
+  id: 'visuallook', // id/label not required, but nice for testing and keeping manifests in sync
+    label: 'visuallook',
     options: {
         title: {
             type: 'string',
